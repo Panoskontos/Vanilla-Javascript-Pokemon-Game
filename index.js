bbb38e75202e5cfc6b659af0004e9e372b58fe93
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-14 15:28:32 +0200

    adding pokemon selection

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ function play() {
 // from select screen choose the pokemons
 var pokemons_el = document.querySelector('.select-screen').querySelectorAll('.character')
 console.log(pokemons_el)
+
+var i=0
+while (i<pokemons_el.length) {
+    pokemons_el[i].onclick = function() {
+        var pokemon_name = this.querySelector('.title').textContent
+        console.log('i choose '+ pokemon_name)
+    }
+    i++
+}
